e77128cfc597ee2d1ba1e414cd7cefe07e6b6361
shubham33159 <shubham33159@gmailcom>
2026-06-28 16:31:34 +0530

    more changes added

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from book_outlet.models import Book, Author, Address, Country
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author","rating",)
+    list_display = ("title","rating","author")
+
+    # list_display = ("title","rating","author__first_name", "author__last_name")
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import get_object_or_404, render
 from book_outlet.models import Book
 from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    books_count = books.count()
+    avg_books = books.aggregate(Avg("rating"))
+
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "books_count": books_count,
+        "avg_books" : avg_books
     })
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     return Http404
 
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title" : book.title,
         "author" : book.author,
